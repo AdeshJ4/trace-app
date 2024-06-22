6aee6008e8a58dbe42e227fea6f3eaff1100f07a
Adesh Jadhav <adejad1234@gmail.com>
2024-06-22 22:11:49 +0530

    Routing and Navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,25 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <h1>Hello Next Js</h1>;
+  return (
+    <main>
+      <ul>
+        <li>
+          <Link href="/home">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link href="/users">Users</Link>
+        </li>
+        <li>
+          <Link href="/users/new">New User</Link>
+        </li>
+      </ul>
+    </main>
+  );
 }
---
+++ app/users/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UsersPage = () => {
+  return <div>List Of Users</div>;
+};
+
+export default UsersPage;
