b1c8464ba54fa64ee9c1cfb4ea331d40f5089797
Adesh Jadhav <adejad1234@gmail.com>
2024-06-23 11:20:58 +0530

    CSS Modules

--- app/components/ProductCard/ProductCard.tsx
+++ app/components/ProductCard/ProductCard.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import AddToCart from "./AddToCart";
+import AddToCart from "../AddToCart/AddToCart";
+import styles from "./ProductCard.module.css";
 
 const ProductCard = () => {
   return (
-    <>
+    <div className={styles.card}>
       <h1>Name: 'Tomato'</h1>
       <h2>Category: Vegetable</h2>
       <AddToCart />
-    </>
+    </div>
   );
 };
 
