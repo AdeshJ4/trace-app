962ff6ef1158e26d03e7881ee53f34f7274e57c6
Adesh Jadhav <adejad1234@gmail.com>
2024-06-23 09:38:55 +0530

    Static and Dynamic Rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -5,18 +5,16 @@ interface User {
   name: string;
 }
 
-// only accessible in fetch function and not in other third party libraries like axios.
-// cache: "no-store" -> Don't store data in cache, fresh data
-// next: {revalidate: 10} -> after 10 sec get fresh data
 const UsersPage = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/users", {
-    next: { revalidate: 10 },
+    cache: "no-store",
   });
   const users: User[] = await response.json();
 
   return (
     <div>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
