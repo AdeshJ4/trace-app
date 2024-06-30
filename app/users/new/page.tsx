ea2c073570c7c1f8330254dfd89dbc1a8eb840e1
Adesh Jadhav <adejad1234@gmail.com>
2024-06-30 21:42:05 +0530

    router() - Navigate to another page

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+'use client';
+
+import { useRouter } from "next/navigation";
 
 const NewUser = () => {
-  return <div>New User Page</div>;
+  const router = useRouter();
+  return (
+    <>
+      <h1>Add User Page</h1>
+
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+
+      <button onClick={() => router.push('/users')} className="btn btn-primary ml-3">Create</button>
+    </>
+  );
 };
 
 export default NewUser;
