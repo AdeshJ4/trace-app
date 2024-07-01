7d46cb08fa16bc35eaf8800cd5b40f1db0858ad5
Adesh Jadhav <adejad1234@gmail.com>
2024-07-01 09:50:37 +0530

    Update user

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -9,6 +9,16 @@ export function GET(request: NextRequest, { params: { id } }: Props) {
     if (id > 10) {
         return NextResponse.json({ error: 'User not found' }, { status: 404 })
     }
-
+    
     return NextResponse.json({ id: 101, fname: 'Adesh' })
+}
+
+
+// visit : http://localhost:3000/api/users/1
+// put -> replacing whole object
+// patch -> replacing some properties
+export async function PUT(request: NextRequest, { params: { id } }: { params: { id: number } }) {
+    const body = await request.json();
+    if (!body.name) return NextResponse.json({ error: "Name is required" }, { status: 400 });
+    return NextResponse.json({id: 101, fname: body.name});
 }
