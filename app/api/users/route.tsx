5702a57eb0e64355fe7046e8bdd6b69c49f6fa80
Adesh Jadhav <adejad1234@gmail.com>
2024-07-01 09:41:21 +0530

    post user api

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -5,9 +5,20 @@ import { NextRequest, NextResponse } from "next/server";
 although we don't use request parameter at this point, if you remove it then next js will cache the response and next time when
 you make the req you will get cache result. so write request.
 */
+
+// visit : http://localhost:3000/api/users
 export function GET(request: NextRequest) {
     return NextResponse.json([
         { id: 101, name: 'Adesh' },
         { id: 102, name: 'Akshay' },
     ])
+}
+
+
+// visit : http://localhost:3000/api/users
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    // validate request
+    if (!body.name) return NextResponse.json({ error: 'Name is required' }, { status: 400 });
+    return NextResponse.json({ ...body }, { status: 201 });
 }
