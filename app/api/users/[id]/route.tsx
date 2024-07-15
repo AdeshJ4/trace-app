3238cc73c09f35b4a73d814ba8fc7bf41cd5b1b3
Adesh Jadhav <adejad1234@gmail.com>
2024-07-15 11:28:20 +0530

    Updating User
    -> updating user using prisma.

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -23,16 +23,28 @@ export async function GET(request: NextRequest, { params }: Props) {
 // visit : http://localhost:3000/api/users/1
 // put -> replacing whole object
 // patch -> replacing some properties
-export async function PUT(request: NextRequest, { params: { id } }: { params: { id: number } }) {
+export async function PUT(request: NextRequest, { params }: Props) {
     const body = await request.json();
     const validation = schema.safeParse(body);
     if (!validation.success) return NextResponse.json({ error: validation.error.errors }, { status: 400 });
-        return NextResponse.json({ id: 101, fname: body.name }, { status: 200 });
-    }
 
+    const user = await prisma.user.findUnique({
+        where: {
+            id: Number(params.id)
+        }
+    });
 
+    if (!user) return NextResponse.json({ error: "User does not exist." }, { status: 404 })
+
+    const updatedUser = await prisma.user.update({
+        where: { id: user.id },
+        data: { name: body.email, email: body.email }
+    });
+
+    return NextResponse.json(updatedUser, { status: 200 });
+}
 
 // visit : http://localhost:3000/api/users/1
-export async function DELETE(request: NextRequest, { params: { id } }: { params: { id: number } }) {
+export async function DELETE(request: NextRequest, { params }: Props) {
     return NextResponse.json({ id: 101, fname: 'Adesh' }, { status: 200 })
 }
