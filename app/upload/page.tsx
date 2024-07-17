a82f2f8f2639e22e9c87adcb68916ad6e645b728
Adesh Jadhav <adejad1234@gmail.com>
2024-07-17 11:22:59 +0530

    Showing Uploaded Images

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,12 +1,24 @@
 "use client";
-import React from 'react'
-import { CldUploadWidget } from 'next-cloudinary'
+import React, { useState } from 'react'
+import { CldImage, CldUploadWidget } from 'next-cloudinary'
+
+interface CloudinaryResult {
+    public_id: string;
+}
 
 const UploadPage = () => {
+    const [publicId, setPublicId] = useState("");
     return (
-        <CldUploadWidget uploadPreset='iyge4qmn'>
-            {({ open }) => <button onClick={() => open()} className='btn btn-primary'>Upload</button>}
-        </CldUploadWidget>
+        <>
+            {publicId && <CldImage src={publicId} width={270} height={180} alt='Image' />}
+            <CldUploadWidget uploadPreset='iyge4qmn' onSuccess={(result, widget) => {
+                if (result.event !== 'success') return;
+                const info = result.info as CloudinaryResult;
+                setPublicId(info.public_id);
+            }} >
+                {({ open }) => <button onClick={() => open()} className='btn btn-primary'>Upload</button>}
+            </CldUploadWidget>
+        </>
     )
 }
 
